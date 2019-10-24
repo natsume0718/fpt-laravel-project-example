8ff0b8606d099b6395c33615bd79cecf5996ea85
Hùng Hồ <thienhungho@gmail.com>
2019-10-24 13:19:16 +0700

    Add config for sluggable

---
+++ database/migrations/2019_10_19_060547_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->text('content');
+            $table->string('feature_image')->nullable();
+            $table->float('price');
+            $table->integer('discount')->default(0);
+            $table->integer('view')->default(0);
+            $table->unsignedBigInteger('product_category_id')->nullable();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('product_category_id')
+                ->references('id')
+                ->on('product_categories')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
